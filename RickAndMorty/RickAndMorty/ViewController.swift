c5a6d758c5f08bbb4569136fd6154cbcee76dbc1
Leo Shaxb <shaxbazyan001@mail.ru>
2023-11-18 01:11:26 +0400

    initial Commit

---
+++ RickAndMorty/RickAndMorty/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Levon Shaxbazyan on 18.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+        
+    }
+
+
+}
+
